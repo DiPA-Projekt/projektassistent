eddc0596764b8e863b9514ece1d65ea984d7b638
bock <bock@4soft.de>
2021-02-15 07:41:28 +0100

    fix: PA-12 made initial tests work

--- frontend/projects/projektassistent-frontend/src/app/modules/project-manual/tailoring-form/tailoring-form.component.spec.ts
+++ frontend/projects/projektassistent-frontend/src/app/modules/project-manual/tailoring-form/tailoring-form.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TailoringFormComponent } from './tailoring-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import { MatListOption, MatSelectionList } from '@angular/material/list';
+import { MatPseudoCheckboxModule, MatRippleModule } from '@angular/material/core';
 
 describe('TailoringFormComponent', () => {
   let component: TailoringFormComponent;
@@ -8,7 +13,8 @@ describe('TailoringFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TailoringFormComponent],
+      declarations: [TailoringFormComponent, MatIcon, MatToolbar, MatToolbarRow, MatSelectionList, MatListOption],
+      imports: [ReactiveFormsModule, MatRippleModule, MatPseudoCheckboxModule],
     }).compileComponents();
   });
 
