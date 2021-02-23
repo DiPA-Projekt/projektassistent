bef083680a2c8007e95384d845bb9cbf15c9b73d
Dennis Bock <bock@4soft.de>
2021-02-23 11:41:42 +0100

    Feature/pa 5 initial code base (#1)
    * feat: PA-5 initial project structure
    
    * feat: PA-5 added some more .gitignore
    
    * fix: exclude .iml files from github
    
    * fix: PA-5 changed kubernetes server port, removed unnecessary dev-stack settings

---
+++ frontend/projects/projektassistent-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule) },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ server/src/test/java/online/projektassistent/ProjektassistentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+ package online.projektassistent;
+
+ import org.junit.jupiter.api.Test;
+ import org.springframework.boot.test.context.SpringBootTest;
+
+ @SpringBootTest
+ class ProjektassistentBackendApplicationTests {
+
+     @Test
+     void contextLoads() {
+     }
+
+ }
