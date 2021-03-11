6525fb5a7bbe76938766984d52353bacf1640118
Martin Oppitz <github@martinoppitz.com>
2021-03-11 13:39:40 +0100

    chore: prepare deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,1 +1,18 @@
-module.exports = require('@leanup/stack-react/webpack.config');
+module.exports = (...args) => {
+  const config = require('@leanup/stack-react/webpack.config')(...args);
+  const CopyPlugin = require('copy-webpack-plugin');
+
+  if (args[0].WEBPACK_BUILD) {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'public',
+          },
+        ],
+      })
+    );
+  }
+
+  return config;
+};
