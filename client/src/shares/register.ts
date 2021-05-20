650b728cf8dd4b330e83b4a8e4f96ffb52a987e1
Dennis Bock <bock@4soft.de>
2021-05-20 23:17:44 +0200

    Feature/PA-45 adjustments document template generation interface (#132)
    * feat: PA-46 added internal references in project documentation
    
    feat: PA-47 draw images in project documentation through inline html
    
    * feat: PA-45 Checkboxen vor Kapiteln entfernt
    Umstellung der Vorlagen auf echten Tree
    Initial sind alle Vorlagen ausgewählt
    Globaler Switch zur Auswahl der Themenbeschreibung

--- client/src/shares/register.ts
+++ client/src/shares/register.ts
@@ -2,8 +2,11 @@ import { DI } from '@leanup/lib/helpers/injector';
 
 import { StatusApi } from '../../openapi';
 import { ProjekthandbuchService } from '../services/projekthandbuch/service';
+import { ProduktvorlagenService } from '../services/projekthandbuch/produktvorlagen/service';
 
 const service = new ProjekthandbuchService();
+const produktVorlagenService = new ProduktvorlagenService();
 
 DI.register('Projekthandbuch', service);
+DI.register('Produktvorlagen', produktVorlagenService);
 DI.register('StatusApi', new StatusApi());
