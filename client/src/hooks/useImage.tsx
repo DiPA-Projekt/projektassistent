c0369446f58be834d0952e5cddb54a0cac4c1797
bock <bock@4soft.de>
2023-04-11 14:42:50 +0200

    reformat with prettier

--- client/src/hooks/useImage.tsx
+++ client/src/hooks/useImage.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 const useImage = (filePath: string) => {
   const [loading, setLoading] = useState(true);
--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -1,14 +1,13 @@
-import i18next from "i18next";
-import HttpBackend from "i18next-http-backend";
-import {i18nOptions} from "./config";
+import i18next from 'i18next';
+import HttpBackend from 'i18next-http-backend';
+import { i18nOptions } from './config';
 
 const useHttpBackend = process.env.NODE_ENV !== 'test';
 const testResources = {
-  resources: {en: {}, de: {}}
+  resources: { en: {}, de: {} },
 };
 
-let i18n = i18next
-  .createInstance(useHttpBackend ? {...i18nOptions} : {...i18nOptions, ...testResources});
+let i18n = i18next.createInstance(useHttpBackend ? { ...i18nOptions } : { ...i18nOptions, ...testResources });
 
 if (useHttpBackend) {
   i18n = i18n.use(HttpBackend);
